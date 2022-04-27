d1354112f34efefa10c36d50d7cfda7f0c89f308
Maicon Thales <maiconthales34@hotmail.com>
2022-04-27 12:00:46 -0300

    Alterar o tempo de expiração do Token.

--- 1_Back/Freefood/src/main/java/com/freefood/project/config/TokenProvider.java
+++ 1_Back/Freefood/src/main/java/com/freefood/project/config/TokenProvider.java
@@ -58,7 +58,7 @@ public class TokenProvider implements Serializable {
 
 		return Jwts.builder().setSubject(authentication.getName()).claim(AUTHORITIES_KEY, authorities)
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY * 1000))
+				.setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY))
 				.signWith(SignatureAlgorithm.HS256, SIGNING_KEY).compact();
 	}
 
