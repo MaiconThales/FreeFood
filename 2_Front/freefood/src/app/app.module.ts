3114d568c99053736b3f1336692a82da9de4299d
Maicon Thales <maiconthales34@hotmail.com>
2022-04-28 10:04:52 -0300

    Add forms no module angular.

--- 2_Front/freefood/src/app/app.module.ts
+++ 2_Front/freefood/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,9 @@ import { TestComponent } from './components';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
